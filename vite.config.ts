ae67bdda07dee328eaf0715f329feb0ffe22e2ab
ralfa <ralfanta0112@gmail.com>
2025-09-14 20:31:45 +0800

    Force cache bust - trigger new deployment

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig(({ mode }) => ({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
       },
     },
   },
