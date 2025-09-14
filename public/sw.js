3b55a7c86191f96371432ed2645a37ed42de8780
ralfa <ralfanta0112@gmail.com>
2025-09-14 20:50:01 +0800

    Force service worker cache clear to fix refresh issues

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'event-connect-v1';
+const CACHE_NAME = 'event-connect-v2-' + Date.now();
 const urlsToCache = [
   '/participant-dashboard',
   '/static/js/bundle.js',
@@ -9,11 +9,31 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+  // Force immediate activation
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      // Force immediate control of all pages
+      return self.clients.claim();
+    })
   );
 });
 
